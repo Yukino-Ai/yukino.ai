f92a4d81ade8db21a2880da49ca39b239ce33fdb
Yukino-Ai <90049783+Yukino-Ai@users.noreply.github.com>
2022-02-15 12:14:27 -0600

    📦 Move old content to separate folder

---
+++ old/blackIce/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<div class="font-size">Hi</div>
+
+<style>
+  /* .bgColor {
+    background-color: none;
+  } */
+
+  .font-size {
+    font-size: 50px;
+  }
+
+  .set-height {
+    height: 15vh;
+    min-height: 15vh;
+  }
+</style>
